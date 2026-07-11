78525a19f1ca680fbeb2b3ba3cd44bf52bb2a87d
kueshima <kazuro.ueshima@gmail.com>
2026-07-11 10:53:28 +0900

    Make lesson science phenomenon focused

--- lessons/ep-book1-041-10/generate_panel_images.py
+++ lessons/ep-book1-041-10/generate_panel_images.py
@@ -46,7 +46,7 @@ def head(draw, x, y, r=16, look="right"):
         draw.ellipse((x + 3, y - 4, x + 7, y), fill=BLACK)
 
 
-def aki(draw, x, y, pose="stand", look="right"):
+def aki(draw, x, y, pose="see", look="right"):
     head(draw, x, y, look=look)
     for sx, ex in [(-14, -22), (-7, -10), (0, 0), (7, 10), (14, 22)]:
         line(draw, x + sx, y - 14, x + ex, y - 29, width=2)
@@ -54,14 +54,11 @@ def aki(draw, x, y, pose="stand", look="right"):
     hip = y + 88
     line(draw, x, neck, x, hip)
     if pose == "put":
-        line(draw, x, y + 43, x + 58, y + 68)
+        line(draw, x, y + 43, x + 65, y + 70)
         line(draw, x, y + 43, x + 22, y + 35)
-    elif pose == "take":
-        line(draw, x, y + 43, x + 68, y + 48)
-        line(draw, x, y + 43, x + 35, y + 65)
-    elif pose == "see":
-        line(draw, x, y + 42, x - 25, y + 62)
-        line(draw, x, y + 42, x + 25, y + 62)
+    elif pose == "hold":
+        line(draw, x, y + 43, x + 70, y + 50)
+        line(draw, x, y + 43, x + 30, y + 68)
     else:
         line(draw, x, y + 42, x - 25, y + 62)
         line(draw, x, y + 42, x + 25, y + 62)
@@ -69,7 +66,7 @@ def aki(draw, x, y, pose="stand", look="right"):
     line(draw, x, hip, x + 32, y + 145)
 
 
-def meg(draw, x, y, pose="stand", look="left"):
+def meg(draw, x, y, pose="see", look="left"):
     head(draw, x, y, look=look)
     tail_x = x + 23 if look == "left" else x - 23
     draw.ellipse((tail_x - 8, y - 6, tail_x + 8, y + 10), outline=BLACK, width=LW)
@@ -78,14 +75,11 @@ def meg(draw, x, y, pose="stand", look="left"):
     line(draw, x, neck, x, waist)
     draw.polygon([(x - 28, waist), (x + 28, waist), (x + 39, waist + 33), (x - 39, waist + 33)], outline=BLACK, fill=WHITE)
     if pose == "put":
-        line(draw, x, y + 43, x - 58, y + 68)
-        line(draw, x, y + 43, x - 26, y + 40)
-    elif pose == "take":
-        line(draw, x, y + 43, x - 68, y + 48)
-        line(draw, x, y + 43, x - 35, y + 65)
-    elif pose == "see":
-        line(draw, x, y + 42, x - 25, y + 62)
-        line(draw, x, y + 42, x + 25, y + 62)
+        line(draw, x, y + 43, x - 65, y + 70)
+        line(draw, x, y + 43, x - 25, y + 40)
+    elif pose == "hold":
+        line(draw, x, y + 43, x - 70, y + 50)
+        line(draw, x, y + 43, x - 30, y + 68)
     else:
         line(draw, x, y + 42, x - 25, y + 62)
         line(draw, x, y + 42, x + 25, y + 62)
@@ -93,36 +87,47 @@ def meg(draw, x, y, pose="stand", look="left"):
     line(draw, x + 12, waist + 33, x + 32, y + 152)
 
 
-def box(draw, x, y, w=150, h=85, water=False):
-    draw.polygon([(x, y), (x + w, y), (x + w - 25, y + h), (x - 25, y + h)], outline=BLACK, fill=WHITE)
-    line(draw, x, y, x - 25, y + h)
-    line(draw, x + w, y, x + w - 25, y + h)
+def table(draw):
+    line(draw, 165, 260, 505, 260, width=2)
+    line(draw, 205, 260, 190, 315, width=2)
+    line(draw, 465, 260, 480, 315, width=2)
+
+
+def glass(draw, x, y, w=105, h=120, water=True):
+    top_left = x + 10
+    top_right = x + w - 10
+    bottom_left = x + 25
+    bottom_right = x + w - 25
+    draw.arc((top_left, y - 7, top_right, y + 15), 0, 360, fill=BLACK, width=LW)
+    line(draw, top_left, y + 4, bottom_left, y + h)
+    line(draw, top_right, y + 4, bottom_right, y + h)
+    draw.arc((bottom_left, y + h - 12, bottom_right, y + h + 10), 0, 180, fill=BLACK, width=LW)
     if water:
-        line(draw, x + 12, y + h // 2, x + w - 38, y + h // 2 - 2, width=2)
-        draw.arc((x + 28, y + h // 2 - 8, x + w - 48, y + h // 2 + 18), 10, 170, fill=BLACK, width=2)
+        wy = y + 62
+        line(draw, top_left + 12, wy, top_right - 12, wy, width=2)
+        draw.arc((top_left + 18, wy - 8, top_right - 18, wy + 16), 10, 170, fill=BLACK, width=2)
+
+
+def pen(draw, x, y, length=118, angle=0, width=9):
+    if angle == 0:
+        body = [(x, y), (x + length, y + 7), (x + length - 2, y + width + 7), (x - 2, y + width)]
+        tip = [(x + length, y + 7), (x + length + 17, y + 13), (x + length - 2, y + width + 7)]
     else:
-        line(draw, x + 18, y + h // 2 + 15, x + w - 40, y + h // 2 + 13, width=2)
+        body = [(x, y), (x + length, y + 36), (x + length - 5, y + 47), (x - 5, y + 11)]
+        tip = [(x + length, y + 36), (x + length + 17, y + 40), (x + length - 5, y + 47)]
+    draw.polygon(body, outline=BLACK, fill=WHITE)
+    draw.polygon(tip, outline=BLACK, fill=BLACK)
+    line(draw, x + 10, y + 2, x + 6, y + width + 2, width=2)
 
 
-def book(draw, x, y, w=76, h=42):
-    draw.rounded_rectangle((x, y, x + w, y + h), radius=5, outline=BLACK, width=LW, fill=WHITE)
-    line(draw, x + 13, y + 4, x + 13, y + h - 4, width=4)
-    for off in (13, 23, 32):
-        line(draw, x + 23, y + off, x + w - 8, y + off - 2, width=1)
+def short_pen_in_glass(draw, x, y):
+    pen(draw, x, y, length=54, angle=0, width=8)
 
 
-def pen(draw, x, y, length=58, angle=1):
-    if angle:
-        p1 = (x, y)
-        p2 = (x + length, y + 22)
-        p3 = (x + length - 4, y + 31)
-        p4 = (x - 4, y + 9)
-        draw.polygon([p1, p2, p3, p4], outline=BLACK, fill=WHITE)
-        draw.polygon([(x + length, y + 22), (x + length + 14, y + 24), (x + length + 4, y + 33)], outline=BLACK, fill=BLACK)
-        line(draw, x + 8, y + 3, x + 3, y + 12, width=2)
-    else:
-        draw.polygon([(x, y), (x + length, y + 8), (x + length - 2, y + 18), (x - 2, y + 10)], outline=BLACK, fill=WHITE)
-        draw.polygon([(x + length, y + 8), (x + length + 14, y + 14), (x + length - 1, y + 18)], outline=BLACK, fill=BLACK)
+def broken_pen(draw, x, y):
+    # The offset lower half makes the straight pen look broken at the water line.
+    pen(draw, x, y, length=76, angle=1, width=8)
+    pen(draw, x + 82, y + 58, length=58, angle=1, width=8)
 
 
 def save(img, name):
@@ -132,42 +137,39 @@ def save(img, name):
 def panel1():
     img = canvas()
     d = ImageDraw.Draw(img)
-    aki(d, 95, 74, pose="put", look="right")
-    meg(d, 548, 74, pose="put", look="left")
-    box(d, 225, 160, w=150, h=95, water=True)
-    book(d, 438, 182, w=80, h=44)
-    pen(d, 278, 152, length=56, angle=1)
-    pen(d, 456, 154, length=56, angle=1)
-    dashed(d, (112, 72), (305, 174))
-    dashed(d, (528, 72), (485, 176))
+    aki(d, 95, 74, pose="see", look="right")
+    meg(d, 548, 74, pose="see", look="left")
+    table(d)
+    pen(d, 230, 225, length=140, angle=0)
+    glass(d, 390, 132, water=True)
+    dashed(d, (112, 72), (300, 230))
+    dashed(d, (528, 72), (430, 175))
     save(img, "panel-1.png")
 
 
 def panel2():
     img = canvas()
     d = ImageDraw.Draw(img)
-    aki(d, 95, 74, pose="see", look="right")
+    aki(d, 95, 74, pose="put", look="right")
     meg(d, 548, 74, pose="see", look="left")
-    box(d, 225, 160, w=150, h=95, water=True)
-    book(d, 438, 182, w=80, h=44)
-    pen(d, 282, 170, length=56, angle=1)
-    pen(d, 456, 154, length=56, angle=1)
-    dashed(d, (112, 72), (305, 185))
-    dashed(d, (528, 72), (485, 176))
+    table(d)
+    glass(d, 305, 126, water=True)
+    short_pen_in_glass(d, 328, 205)
+    dashed(d, (112, 72), (350, 210))
+    dashed(d, (528, 72), (352, 210))
     save(img, "panel-2.png")
 
 
 def panel3():
     img = canvas()
     d = ImageDraw.Draw(img)
-    aki(d, 95, 74, pose="take", look="right")
-    meg(d, 548, 74, pose="take", look="left")
-    box(d, 225, 160, w=150, h=95, water=True)
-    book(d, 438, 182, w=80, h=44)
-    pen(d, 388, 160, length=56, angle=1)
-    pen(d, 250, 120, length=56, angle=1)
-    dashed(d, (112, 72), (278, 142))
-    dashed(d, (528, 72), (414, 180))
+    aki(d, 95, 74, pose="see", look="right")
+    meg(d, 548, 74, pose="hold", look="left")
+    table(d)
+    glass(d, 280, 132, water=True)
+    pen(d, 360, 205, length=130, angle=0)
+    dashed(d, (112, 72), (430, 210))
+    dashed(d, (528, 72), (430, 210))
     save(img, "panel-3.png")
 
 
@@ -176,11 +178,11 @@ def panel4():
     d = ImageDraw.Draw(img)
     aki(d, 95, 74, pose="see", look="right")
     meg(d, 548, 74, pose="see", look="left")
-    box(d, 225, 160, w=150, h=95, water=True)
-    pen(d, 180, 150, length=56, angle=1)
-    pen(d, 284, 170, length=56, angle=1)
-    dashed(d, (112, 72), (205, 172))
-    dashed(d, (528, 72), (310, 190))
+    table(d)
+    glass(d, 305, 126, water=True)
+    broken_pen(d, 274, 164)
+    dashed(d, (112, 72), (362, 218))
+    dashed(d, (528, 72), (362, 218))
     save(img, "panel-4.png")
 
 
